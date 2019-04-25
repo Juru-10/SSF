16b561c8d551407f0ad62210f8dd11eb5175c5b2
mbabazif <mbabaziflorence81@gmail.com>
2019-04-25 19:04:40 +0200

    adding the templates

--- virtual/bin/activate.fish
+++ virtual/bin/activate.fish
@@ -29,7 +29,7 @@ end
 # unset irrelevant variables
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV "/home/wecode/SSF/virtual"
+set -gx VIRTUAL_ENV "/home/florence/Documents/SSF/virtual"
 
 set -gx _OLD_VIRTUAL_PATH $PATH
 set -gx PATH "$VIRTUAL_ENV/bin" $PATH
