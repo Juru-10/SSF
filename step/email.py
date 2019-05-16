c8b04b9ff3ebeb91e55bbe35012deeb30369c8ab
JURU <jurassu10@gmail.com>
2019-05-16 20:17:50 +0200

    add email notification

--- step/email.py
+++ step/email.py
@@ -4,16 +4,23 @@ import requests
 
 def marks_message(name,receiver):
     print(name)
+    requests.post(
+        "https://api.mailgun.net/v3/sandbox8b74f612789643efbc5203e7719c6e3b.mailgun.org/messages",
+        auth=("api", "125b41a486644fbfb16daac83ec81854-4a62b8e8-22c3747c"),
+        data={"from": "Mailgun Sandbox <postmaster@sandbox8b74f612789643efbc5203e7719c6e3b.mailgun.org>",
+              "to": "aristote <aristotek96@gmail.com>",
+              "subject": "Hello aristote",
+              "text": "Congratulations aristote, you just sent an email with Mailgun!  You are truly awesome!"})
     # Creating message subject and sender
     # subject = 'Welcome to SSF'
     # sender = 'juruassumpta@zoho.com'
-    print(requests.post(
-        "https://api.mailgun.net/v3/sandbox3b2294b871f94f64a36ffc2fbfaeda8a.mailgun.org/messages",
-        auth=("api", "12b331c378fdbdbb5e141eb368ae2200-e566273b-fd988a9e"),
-        data={"from": "SSF <postmaster@sandbox3b2294b871f94f64a36ffc2fbfaeda8a.mailgun.org>",
-              "to": receiver,
-              "subject": "Hello Assumpta Uwanyirijuru",
-              "text": "New Marks for "+name}))
+    # return requests.post(
+    #     "https://api.mailgun.net/v3/sandbox3b2294b871f94f64a36ffc2fbfaeda8a.mailgun.org/messages",
+    #     auth=("api", "12b331c378fdbdbb5e141eb368ae2200-e566273b-fd988a9e"),
+    #     data={"from": "SSF <postmaster@sandbox3b2294b871f94f64a36ffc2fbfaeda8a.mailgun.org>",
+    #           "to": "<jurassu10@gmail.com>",
+    #           "subject": "Hello Assumpta Uwanyirijuru",
+    #           "text": "New Marks for "+name})
 
     #passing in the context vairables
     # text_content = render_to_string('email/marks.txt',{"name": name})
